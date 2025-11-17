078ae4865868db43ea0028b7cd4feca5af5fb5be
Gopal Mondal <gopalmandol439@gmail.com>
2025-11-17 18:06:20 +0530

    Updating side navbar

---
+++ Capstone-project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Sidebar from './Component/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import ChatBox from './Component/ChatBox'
+import Community from './Pages/Community'
+const App = () => {
+  return (
+    <>
+      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ Capstone-project/src/Pages/Loadin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loadin = () => {
+  return (
+    <div>Loadin</div>
+  )
+}
+
+export default Loadin
